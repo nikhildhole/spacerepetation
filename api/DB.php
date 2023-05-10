99e3fdd7abddd19c9e58558c27fffc94d8c8b949
Nikhil Dhole <95525442+nikhildhole@users.noreply.github.com>
2023-05-10 20:45:49 +0530

    Update DB.php

--- api/DB.php
+++ api/DB.php
@@ -1,15 +1,44 @@
 <?php
-$serverName = "localhost";
-$userName = "spacerepetation";
-$password = "sprite";
-$dbName = "spacerepetation";
-$connection;
 
-try {
-    $connection = new PDO("mysql:host=".$serverName.";dbname=".$dbName, $userName, $password);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+class DB{
+    static private $serverName = "localhost";
+    static private $userName = "spacerepetation";
+    static private $password = "sprite";
+    static private $dbName = "spacerepetation";
+    static private $conn;
+
+    function __construct(){
+        try {
+            $conn = new PDO("mysql:host=".$serverName.";dbname=".$dbName, $userName, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+    
+    function selectQuery($query = null, $param[] = null){
+        try{
+            $stmt = $conn->prepare($query);
+            $stmt->execute($param[]);
+            $result = $stmt->fetchAll();
+            return $result;
+        } catch(PDOException $e){
+            die($e->getMessage());
+        }
+    }
+    
+    function executeQuery($query = null, $param[] = null){
+        try{
+            $stmt = $conn->prepare($query);
+            $stmt->execute($param[]);
+        } catch(PDOException $e){
+            die($e->getMessage());
+        }
+    }
+    
+    function __destruct(){
+        $conn = null;
+    }
+
 }
 ?>
